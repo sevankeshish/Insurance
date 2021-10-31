be2dbbe78e9eaaa13805fc2a50f090daae1395d1
Sevan Keshish <35175803+sevankeshish@users.noreply.github.com>
2021-10-31 14:07:49 +0330

    access to the form

--- Insurance/js/app.js
+++ Insurance/js/app.js
@@ -1,44 +1,57 @@
 //variables
-
+const form = document.querySelector("#request-quote")
 
 
 //eventListeners
-document.addEventListener("DOMContentLoaded" , function (){
+eventListeners()
 
-    //display the <option>
-   const html = new HTMLUI()
-// console.log(html)
-html.displayYears()
+function eventListeners() {
 
-})
+    document.addEventListener("DOMContentLoaded", function () {
 
+        //display the <option>
+        const html = new HTMLUI()
+        // console.log(html)
+        html.displayYears()
 
+    })
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault()
+
+        const make = document.querySelector("#make").value
+        const year = document.getElementById("year").value
+        const level = document.querySelector('input[name="level"]:checked').value
+        // console.log(make);
+        // console.log(year);
+        // console.log(level);
+
+    })
+
+}
 
 
 //objects
-function HTMLUI(){} 
+function HTMLUI() {}
 
 //display year
-HTMLUI.prototype.displayYears = function(){
+HTMLUI.prototype.displayYears = function () {
     let
-persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g],
-arabicNumbers  = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g],
-fixNumbers = function (str)
-{
-  if(typeof str === 'string')
-  {
-    for(let i=0; i<10; i++)
-    {
-      str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
-    }
-  }
-  return str;
-};
+        persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g],
+        arabicNumbers = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g],
+        fixNumbers = function (str) {
+            if (typeof str === 'string') {
+                for (let i = 0; i < 10; i++) {
+                    str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
+                }
+            }
+            return str;
+        };
 
     //get max year
     const now = new Date().toLocaleDateString('fa-IR');
     // console.log(year)
-    let nowYear = now.slice(0,4)
+    let nowYear = now.slice(0, 4)
     let max = fixNumbers(nowYear)
     // console.log(max);
 
@@ -49,14 +62,14 @@ fixNumbers = function (str)
     const selectYear = document.querySelector("#year")
 
     //create for loop for maing the option tag
-    for (let i= max; i >= min; i--) {
+    for (let i = max; i >= min; i--) {
         //create option
         const option = document.createElement("option")
-        option.value = i 
+        option.value = i
         option.innerText = i
 
         //append option to the selectYear
         selectYear.appendChild(option)
-        
+
     }
 }
